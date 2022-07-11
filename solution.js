dd5a93f096d3cb2c33e1d0da36da0774f08186c0
LauraMenzel <menzel-laura@gmx.de>
2022-07-11 22:02:41 +0200

    adding ternary exercise

---
+++ solution.js
@@ -0,0 +1,67 @@
+//1.
+const isDog = true;
+if (isDog === true) {console.log("pat, pat");}
+else{ console.log("find me a dog to pat!");}
+
+console.log("---------------------------------------------------------");
+
+
+//2.
+const speedLimit = 50
+const yourSpeed = 70
+
+if (yourSpeed > speedLimit)
+{console.log('you are going too fast!' );}
+else
+{console.log('You are driving below the speed limit, Oma'); }
+
+console.log("---------------------------------------------------------");
+
+//3.
+const age = 20
+
+if (age >= 16)
+{console.log("serve beer");}
+
+else 
+{console.log("serve butter beer");}
+
+console.log("---------------------------------------------------------");
+
+//4.
+const isStudent = false;
+if (isStudent === true) {console.log("Ticket costs €5,00");}
+
+else {console.log("Ticket costs €12,00");}
+
+
+console.log("---------------------------------------------------------");
+
+//5.
+
+const okMarie = "flower"
+
+
+if (okMarie === "cake")
+{console.log("Let them eat cake");}
+
+else {console.log("Oh, bother");}
+
+console.log("---------------------------------------------------------");
+
+//6.
+const num1 = 30
+const num2 = 939
+const num3 = 40.9
+
+console.log((num1 % 2 == 0) ? `${num1} is even` : `${num1} is odd`);
+
+console.log(num2 % 2 == 0 ? `${num2} is even` : `${num2} is odd`);
+
+console.log(num3 % 2 == 0 ? `${num3} is even` : `${num3} is odd`);
+
+// if (num1 % 2 == 0) {
+//   console.log(`${num1} is even!`);
+// } else {
+//   console.log(`${num1} is odd!`);
+// }
